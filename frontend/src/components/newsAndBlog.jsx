6ae798c30ffe5e8c9acb6a13a09f6271e36c34f4
Prabin Joshi <joshiprabin17@gmail.com>
2025-03-16 23:12:00 +0545

    updated by prabin: news & blog section

--- frontend/src/components/newsAndBlog.jsx
+++ frontend/src/components/newsAndBlog.jsx
@@ -3,23 +3,23 @@ import React from 'react'
 
 export default function NewsAndBlog() {
     return (
-        <div className='p-20'>
+        <div className='p-4 md:p-10 lg:p-20'>
             {/* first child */}
-            <div className="font-serif flex items-center justify-between flex-col">
+            <div className="font-serif flex items-center justify-between flex-col text-center md:text-left">
                 <div className="flex justify-between items-center space-x-2">
                     <MoveLeft className='text-red-500'></MoveLeft>
                     <p className='text-xl text-red-500'>NEWS & BLOG</p>
                     <MoveRight className='text-red-500'></MoveRight>
                 </div>
 
-                <span className="text-5xl font-semibold">Our Latest News & Blog</span>
+                <span className="text-3xl md:text-4xl lg:text-5xl font-semibold">Our Latest News & Blog</span>
             </div>
 
             {/* second child */}
-            <div className='grid grid-cols-3 cursor-pointer mt-15'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 cursor-pointer mt-10 gap-6'>
                 {/* second: left child */}
                 <div className='relative object-cover space-y-3'>
-                    <img src="/burger.jpg" alt="" className='w-85 rounded-md' />
+                    <img src="/burger.jpg" alt="" className='w-full rounded-md' />
                     <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>BURGER</p>
 
                     <p className='font-semibold text-lg hover:text-red-500 hover:underline'>Announcing if attachment resolution sentiments at the projection.</p>
@@ -28,7 +28,7 @@ export default function NewsAndBlog() {
 
                 {/* second: middle child */}
                 <div className='relative object-cover space-y-3'>
-                    <img src="/food.jpg" alt="" className='w-85 rounded-md' />
+                    <img src="/food.jpg" alt="" className='w-full rounded-md' />
                     <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>FOOD</p>
 
                     <p className='font-semibold text-lg hover:text-red-500 hover:underline'>This prefabricated passive are comfortable highly sustainable.</p>
@@ -37,7 +37,7 @@ export default function NewsAndBlog() {
 
                 {/* second: right child */}
                 <div className='relative object-cover space-y-3'>
-                    <img src="/pizza.jpg" alt="" className='w-85 rounded-md' />
+                    <img src="/pizza.jpg" alt="" className='w-full rounded-md' />
                     <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>PIZZA</p>
 
                     <p className='font-semibold text-lg hover:text-red-500 hover:underline'>Minuter him own clothes but observe country at the maintaining.</p>
