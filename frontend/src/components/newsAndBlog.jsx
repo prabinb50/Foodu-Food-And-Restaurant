5456e5a9aaf9ab6b1dad762e48b547a6c5e6b247
Prabin Joshi <joshiprabin17@gmail.com>
2025-03-23 13:09:16 +0545

    updated by prabin: fetch the News&Blog data from the backendgit commit -m

--- frontend/src/components/newsAndBlog.jsx
+++ frontend/src/components/newsAndBlog.jsx
@@ -1,7 +1,30 @@
 import { Dot, MoveLeft, MoveRight } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function NewsAndBlog() {
+
+    // state to store the news and blog data 
+    const [newsAndBlog, setNewsAndBlog] = useState(null);
+
+    // fetch the news and blog data from the backend
+    const fetchNewsAndBlog = async () => {
+        try {
+            const response = await axios.get("http://localhost:4000/newsAndBlog");
+            // console.log("response", response.data.data);
+            setNewsAndBlog(response.data.data);
+
+        } catch (error) {
+            console.log("Something went wrong while fetching the news and blog data", error);
+        }
+    }
+
+    // call the fetchNewsAndBlog function
+    useEffect(() => {
+        fetchNewsAndBlog();
+    }, []);
+
+
     return (
         <div className='w-11/12 md:w-10/12 lg:w-10/12 mx-auto mt-10 md:mt-20 pb-20'>
             {/* first child */}
@@ -17,32 +40,35 @@ export default function NewsAndBlog() {
 
             {/* second child */}
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 cursor-pointer mt-10 gap-6'>
-                {/* second: left child */}
-                <div className='relative object-cover space-y-3'>
-                    <img src="/burger.jpg" alt="" className='w-full rounded-md' />
-                    <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>BURGER</p>
+                {
+                    newsAndBlog?.map((eachItem, index) => (
+                        <div key={index} className='relative object-cover space-y-3'>
+                            <img src={eachItem.image} alt="" className='w-full rounded-md' />
+                            <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>{eachItem.category}</p>
 
-                    <p className='font-semibold text-lg hover:text-red-500 hover:underline'>Announcing if attachment resolution sentiments at the projection.</p>
-                    <p className='opacity-50 font-semibold text-sm flex'>18 MARCH, 2024 <span className='flex hover:text-red-500 hover:opacity-90'><Dot />MD SOHAG</span></p>
-                </div>
+                            <p className='font-semibold text-lg hover:text-red-500 hover:underline'>{eachItem.description}</p>
+                            <p className='opacity-50 font-semibold text-sm flex'>{eachItem.date} <span className='flex hover:text-red-500 hover:opacity-90'><Dot />{eachItem.author}</span></p>
+                        </div>
+                    ))
+                }
 
                 {/* second: middle child */}
-                <div className='relative object-cover space-y-3'>
+                {/* <div className='relative object-cover space-y-3'>
                     <img src="/food.jpg" alt="" className='w-full rounded-md' />
                     <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>FOOD</p>
 
                     <p className='font-semibold text-lg hover:text-red-500 hover:underline'>This prefabricated passive are comfortable highly sustainable.</p>
                     <p className='opacity-50 font-semibold text-sm flex'>18 MARCH, 2024 <span className='flex hover:text-red-500 hover:opacity-90'><Dot />MD SOHAG</span></p>
-                </div>
+                </div> */}
 
                 {/* second: right child */}
-                <div className='relative object-cover space-y-3'>
+                {/* <div className='relative object-cover space-y-3'>
                     <img src="/pizza.jpg" alt="" className='w-full rounded-md' />
                     <p className='absolute top-8 left-5 text-black border-none bg-white rounded-md text-xs px-2 py-1 font-semibold hover:text-red-500'>PIZZA</p>
 
                     <p className='font-semibold text-lg hover:text-red-500 hover:underline'>Minuter him own clothes but observe country at the maintaining.</p>
                     <p className='opacity-50 font-semibold text-sm flex'>18 MARCH, 2024 <span className='flex hover:text-red-500 hover:opacity-90'><Dot />MD SOHAG</span></p>
-                </div>
+                </div> */}
             </div>
         </div>
     )
